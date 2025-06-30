62ff6e818ea8eff8197684e543d38f8501913b03
Liad Peretz <peretz.liad@gmail.com>
2025-06-30 20:37:31 +0200

    Question 2: Sum Two. Performance improved version

--- java/Question2.java
+++ java/Question2.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 // https://leetcode.com/problems/two-sum/
 class Question2 {
@@ -6,15 +7,15 @@ class Question2 {
         int[] nums = new int[] { 2, 7, 11, 15 };
         int target = 9;
         Question2 q = new Question2();
-        System.out.println(Arrays.toString(q.twoSum(nums, target)));
+        System.out.println(Arrays.toString(q.twoSumV2(nums, target)));
 
         nums = new int[] { 3, 2, 4 };
         target = 6;
-        System.out.println(Arrays.toString(q.twoSum(nums, target)));
+        System.out.println(Arrays.toString(q.twoSumV2(nums, target)));
 
         nums = new int[] { 3, 3 };
         target = 6;
-        System.out.println(Arrays.toString(q.twoSum(nums, target)));
+        System.out.println(Arrays.toString(q.twoSumV2(nums, target)));
     }
     
     public int[] twoSum(int[] nums, int target) {
@@ -26,6 +27,24 @@ class Question2 {
                     result[1] = i2;
                     break OuterLoop;
                 }
+            }
+        }
+        return result;
+    }
+
+    public int[] twoSumV2(int[] nums, int target) {
+        HashMap<Integer,Integer> numberCache = new HashMap<>();
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            numberCache.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            Integer indexOfCorresponding = numberCache.get(target - nums[i]);
+            if (indexOfCorresponding != null && indexOfCorresponding != i) {
+                return (indexOfCorresponding > i) ? (new int[] { i, indexOfCorresponding })
+                        : (new int[] { indexOfCorresponding, i });
             }
         }
         return result;
